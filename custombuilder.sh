9f13e9a5a2bcbfade78d10e4ca9038c12fe3bd03
Bitals <me@bitals.xyz>
2022-04-17 14:56:31 +0300

    Install and cleanup

--- custombuilder.sh
+++ custombuilder.sh
@@ -9,5 +9,5 @@ cd $OWNPACKAGE
 makepkg --printsrcinfo > .SRCINFO
 #aur graph .SRCINFO | tsort | tac > queue
 aur build --database Bitals --root /home/builder/bitalsrepo $OWNPACKAGE
-#sudo pacman -Sy --noconfirm $OWNPACKAGE
-#sudo pacman -Sc --noconfirm
+sudo pacman -Sy --noconfirm $OWNPACKAGE
+sudo pacman -Sc --noconfirm
