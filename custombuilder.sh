09f98792ae25289ec2817666c474953589ea0420
Bitals <me@bitals.xyz>
2022-04-17 15:34:55 +0300

    Learn to fail

--- custombuilder.sh
+++ custombuilder.sh
@@ -8,6 +8,6 @@ gpg --fingerprint 5D11E19794FC8007AFE3600CEB70C01D5CEABF2C
 cd $OWNPACKAGE
 makepkg --printsrcinfo > .SRCINFO
 #aur graph .SRCINFO | tsort | tac > queue
-aur build --database Bitals --root /home/builder/bitalsrepo $OWNPACKAGE
+aur build --database Bitals --root /home/builder/bitalsrepo $OWNPACKAGE|| exit 1
 sudo pacman -Sy --noconfirm $OWNPACKAGE
 sudo pacman -Sc --noconfirm
