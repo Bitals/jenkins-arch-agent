7d45b5016ddfc4e8dc26fc05a95f36c75d2de50d
Bitals <me@bitals.xyz>
2022-04-27 21:45:43 +0300

    more forcefully force

--- custombuilder.sh
+++ custombuilder.sh
@@ -8,8 +8,10 @@ gpg --fingerprint 5D11E19794FC8007AFE3600CEB70C01D5CEABF2C
 #cd $OWNPACKAGE
 makepkg --printsrcinfo > .SRCINFO
 #aur graph .SRCINFO | tsort | tac > queue
-#repo-remove /home/builder/bitalsrepo/Bitals.db.tar.gz $OWNPACKAGE
-aur build -f --database Bitals --root /home/builder/bitalsrepo $OWNPACKAGE|| exit 1
+repo-remove /home/builder/bitalsrepo/Bitals.db.tar.gz $OWNPACKAGE
+rm -rf /home/builder/bitalsrepo/$OWNPACKAGE.pkg.tar.zst
+rm -rf /home/builder/bitalsrepo/$OWNPACKAGE.pkg.tar.zst.sig
+aur build --database Bitals --root /home/builder/bitalsrepo $OWNPACKAGE|| exit 1
 sudo pacman -Sy --noconfirm $OWNPACKAGE || exit 1
 sudo pacman -Sc --noconfirm
 sudo kill $( cat /opt/piavpn-manual/pia_pid )
