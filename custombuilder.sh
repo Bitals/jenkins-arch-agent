155f873deb239fb5be335ff2abf4e56a194136e5
Bitals <me@bitals.xyz>
2022-04-30 20:00:56 +0300

    Revert deleting

--- custombuilder.sh
+++ custombuilder.sh
@@ -8,9 +8,11 @@ gpg --fingerprint 5D11E19794FC8007AFE3600CEB70C01D5CEABF2C
 #cd $OWNPACKAGE
 makepkg --printsrcinfo > .SRCINFO
 #aur graph .SRCINFO | tsort | tac > queue
-repo-remove /home/builder/bitalsrepo/Bitals.db.tar.gz $OWNPACKAGE
-rm -rf /home/builder/bitalsrepo/$OWNPACKAGE.pkg.tar.zst
-rm -rf /home/builder/bitalsrepo/$OWNPACKAGE.pkg.tar.zst.sig
+#repo-remove /home/builder/bitalsrepo/Bitals.db.tar.gz $OWNPACKAGE
+# rm -rf /home/builder/bitalsrepo/$OWNPACKAGE.pkg.tar.zst
+# rm -rf /home/builder/bitalsrepo/$OWNPACKAGE.pkg.tar.zst.sig
+# rm -rf /home/builder/
+# rm -rf /home/builder/
 aur build --database Bitals --root /home/builder/bitalsrepo $OWNPACKAGE|| exit 1
 sudo pacman -Sy --noconfirm $OWNPACKAGE || exit 1
 sudo pacman -Sc --noconfirm
