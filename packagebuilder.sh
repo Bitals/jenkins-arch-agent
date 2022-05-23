4002965541a729dd42a944d84ea58448f0b37655
Bitals <me@bitals.xyz>
2022-05-24 01:55:27 +0300

    Forcefully update pacman cache (because I like playing with ZFS)

--- packagebuilder.sh
+++ packagebuilder.sh
@@ -19,7 +19,7 @@ else
     gpg --import "$AURPACKAGE"-key
 fi
 echo Updating pacman databases...
-sudo pacman -Sy
+sudo pacman -Syy
 echo Building "$AURPACKAGE"...
 aur sync -A --noconfirm --noview --sign --database Bitals --root /home/builder/bitalsrepo $AURPACKAGE || exit 1 #sudo kill $( cat /opt/piavpn-manual/pia_pid ) #&& rm -rf /home/builder/.cache/
 sudo pacman -Sc --noconfirm
