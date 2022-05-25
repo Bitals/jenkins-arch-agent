9a0b7fd8b0aac8f8c33e6af7afbd296183eea899
Bitals <me@bitals.xyz>
2022-05-25 12:27:04 +0300

    Deeper VPN check

--- vpn.sh
+++ vpn.sh
@@ -9,6 +9,10 @@ sudo chmod 0666 /dev/net/tun
 echo "Connecting to PIA OpenVPN"
 cd /home/builder/manual-connections
 sudo VPN_PROTOCOL=openvpn_udp_standard DISABLE_IPV6="yes" AUTOCONNECT=true PIA_DNS=true PIA_PF=false PIA_USER=$PIA_USER PIA_PASS=$PIA_PASS ./run_setup.sh || exit 1
+if [[ -z $( cat /opt/piavpn-manual/pia_pid ) ]]; then
+    echo "VPN connectin failed"
+    exit 1
+fi
 echo "Adding route to local network"
 sudo ip route add "192.168.88.0/24" via "172.18.0.1" dev "eth0"
 echo "Public IP: $( curl https://2ip.ru )"
