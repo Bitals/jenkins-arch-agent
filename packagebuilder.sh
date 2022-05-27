d06f352a35c0d3ef366361d96e67a8b1ec353ce1
Bitals <me@bitals.xyz>
2022-05-28 00:26:14 +0300

    Clear cache

--- packagebuilder.sh
+++ packagebuilder.sh
@@ -21,6 +21,6 @@ fi
 echo Updating pacman databases...
 sudo pacman -Syy
 echo Building "$AURPACKAGE"...
-aur sync -A --noconfirm --noview --sign --database Bitals --root /home/builder/bitalsrepo $AURPACKAGE || exit 1 #sudo kill $( cat /opt/piavpn-manual/pia_pid ) #&& rm -rf /home/builder/.cache/
+aur sync -A --noconfirm --noview --sign --database Bitals --root /home/builder/bitalsrepo $AURPACKAGE || exit 1 || rm -rf /home/builder/.cache/aurutils/sync/$AURPACKAGE
 sudo pacman -Sc --noconfirm
 sudo kill $( cat /opt/piavpn-manual/pia_pid )
