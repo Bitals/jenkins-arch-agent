33db0820968c867efe51a1318b543681b4e6781b
Bitals <me@bitals.xyz>
2022-08-14 14:12:43 +0300

    Use internal DNS

--- vpn.sh
+++ vpn.sh
@@ -10,7 +10,7 @@ echo "Connecting to PIA OpenVPN"
 cd /home/builder/manual-connections
 succ=false
 while [[ $succ != true ]]; do
-    sudo VPN_PROTOCOL=openvpn_udp_standard DISABLE_IPV6="yes" AUTOCONNECT=true PIA_DNS=true PIA_PF=false PIA_USER=$PIA_USER PIA_PASS=$PIA_PASS ./run_setup.sh
+    sudo VPN_PROTOCOL=openvpn_udp_standard DISABLE_IPV6="yes" AUTOCONNECT=true PIA_DNS=false PIA_PF=false PIA_USER=$PIA_USER PIA_PASS=$PIA_PASS ./run_setup.sh
     if [[ -z $( cat /opt/piavpn-manual/pia_pid ) ]]; then
         echo "VPN connectin failed, will try again in 5s"
         succ=false
