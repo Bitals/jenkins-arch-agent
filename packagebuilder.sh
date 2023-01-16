a6ba9a538e8ca4ef68d7a5dcc4844adb673639f8
Bitals <me@bitals.xyz>
2023-01-16 23:05:10 +0300

    New static IP

--- packagebuilder.sh
+++ packagebuilder.sh
@@ -2,7 +2,7 @@
 
 #/opt/vpn.sh || exit 1
 pIP=$( curl https://2ip.ru )
-if [[ $pIP == "89.179.246.20" ]]; then
+if [[ $pIP == "93.90.41.145" ]]; then
     echo "Public IP: "$pIP", exiting now"
     exit 1
 else
