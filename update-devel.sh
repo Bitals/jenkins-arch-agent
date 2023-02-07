ea2ca4fc4d0a2489f96145e1291cf4b58aade543
Bitals <me@bitals.xyz>
2023-02-07 23:43:10 +0300

    remove excessive?

--- update-devel.sh
+++ update-devel.sh
@@ -14,20 +14,9 @@ cd /home/builder
 gpg --import $BITALSARK
 gpg --fingerprint 5D11E19794FC8007AFE3600CEB70C01D5CEABF2C
 
-if [[ -z "$PGPFINGER" ]]; then
-    break
-else
-    gpg --recv-keys $PGPFINGER
-fi
-if [[ -z "$PGPLINK" ]]; then
-    break
-else
-    curl $PGPLINK > "$AURPACKAGE"-key
-    gpg --import "$AURPACKAGE"-key
-fi
 echo Updating pacman databases...
 sudo pacman -Syy
-echo Building "$AURPACKAGE"...
-aur sync update-devel -A --noconfirm --noview --sign --database Bitals --root /home/builder/bitalsrepo || exit 1
+echo Updating devel packages...
+aur update-devel --database Bitals --root /home/builder/bitalsrepo
 sudo pacman -Sc --noconfirm
 #sudo kill $( cat /opt/piavpn-manual/pia_pid )
