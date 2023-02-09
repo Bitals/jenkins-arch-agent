5d97b37380f2693bf42bddcab7b82e2a405aec11
Bitals <me@bitals.xyz>
2023-02-09 21:10:03 +0300

    Fuck that case

--- aur-update-devel-fork.sh
+++ aur-update-devel-fork.sh
@@ -10,16 +10,8 @@ fi
 # upgrade vcs packages to latest commits
 if [[ $(aur vercmp-devel "$@" | tee updates) ]]; then
     printf "\n$(column -t updates)\n\n$(wc -l updates) found.  "
-    read -r -p "Proceed? [y/N] " response
-    case "$response" in
-        [yY]|[yY][eE][sS])
-            # If build dependences are not installed, then --noconfirm is
-            # required, or the command will terminate at the y/N prompt for
-            # installing the dependency
-            cut -d\  -f1 updates > vcs.txt
-            xargs -a vcs.txt aur sync --no-ver-argv --noconfirm --noview --sign "$@"
-            ;;
-    esac
+    cut -d\  -f1 updates > vcs.txt
+    xargs -a vcs.txt aur sync --no-ver-argv --noconfirm --noview --sign "$@"
 else
     msg2 "No updates found"
 fi
