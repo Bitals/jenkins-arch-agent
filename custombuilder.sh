c0a80cbc31c22141b696a2e10d5ab1188fe4a9a3
Bitals <me@bitals.xyz>
2023-10-09 20:02:12 +0400

    Explicitly install dependencies for custom packages

--- custombuilder.sh
+++ custombuilder.sh
@@ -12,6 +12,11 @@ cd /home/builder/workspace/AUR/"$OWNPACKAGE" || exit 1
 # rm -rf    $OWNPACKAGE.pkg.tar.zst.sig
 # rm -rf /home/builder/
 # rm -rf /home/builder/
+deps=""
+for i in $( grep "depends" .SRCINFO|cut -d "=" -f 2 ); do
+    deps+="$i "
+done
+sudo pacman -S --noconfirm $deps
 aur build -f --database Bitals --root /home/builder/bitalsrepo $OWNPACKAGE|| exit 1
 sudo pacman -Sy --noconfirm $OWNPACKAGE || exit 1
 #sudo kill $( cat /opt/piavpn-manual/pia_pid )
