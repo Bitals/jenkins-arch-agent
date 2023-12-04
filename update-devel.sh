ec054e766d0e874beb8327f7cdd54f5c5e241b6a
Bitals <me@bitals.xyz>
2023-12-04 21:10:33 +0400

    Force rebuild if source is missing; trim % trailing pkgbuild for some reason; be more verbose

--- update-devel.sh
+++ update-devel.sh
@@ -5,9 +5,11 @@ sudo pacman -Syy
 echo Updating devel packages...
 
 repover=$( pacman -Si "$AURPACKAGE"|grep Version|sed -E 's/Version         : //' )
-cd /home/builder/.cache/aurutils/sync/"$AURPACKAGE"||exit
+cd /home/builder/.cache/aurutils/sync/"$AURPACKAGE"|| /opt/rebuilder.sh
 makepkg -o
-gitver=$( bash -c "source PKGBUILD && pkgver" )
+gitver=$( bash -c "source PKGBUILD && echo ${pkgver//%}" )
+echo Repo: $repover
+echo Source: $gitver
 if [[ "$repover" != "$gitver" ]]; then
     /opt/rebuilder.sh || exit 1
 else
