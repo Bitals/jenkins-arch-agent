0dd8984fda936d604f7b4002e658b662f95cabec
Bitals <me@bitals.xyz>
2023-12-04 22:03:29 +0400

    Replace placeholder for an actual check and solution

--- update-devel.sh
+++ update-devel.sh
@@ -4,13 +4,17 @@ echo Updating pacman databases...
 sudo pacman -Syy
 echo Updating devel packages...
 
-repover=$( pacman -Si "$AURPACKAGE"|grep Version|sed -E 's/Version         : //' )
-cd /home/builder/.cache/aurutils/sync/"$AURPACKAGE"|| echo "No local source found, rebuilding" && /opt/rebuilder.sh
-makepkg -o
+repover=$( pacman -Si $AURPACKAGE|grep Version|sed -E 's/Version         : //'|cut -d "-" -f 1 )
+if [ ! -d /home/builder/.cache/aurutils/sync/"$AURPACKAGE" ]; then
+    cd /home/builder/.cache/aurutils/sync/|| exit 1
+    git clone https://aur.archlinux.org/"$AURPACKAGE".git || exit 1
+fi
+cd /home/builder/.cache/aurutils/sync/"$AURPACKAGE" || exit 1
+makepkg -do
 gitver=$( grep pkgver= PKGBUILD|cut -d "=" -f 2 )
 echo Repo: $repover
 echo Source: $gitver
-if [[ "$repover" != "$gitver" ]]; then
+if [[ -n "$repover" && "$repover" != "$gitver" ]]; then
     /opt/rebuilder.sh || exit 1
 else
     echo "No updates found"
