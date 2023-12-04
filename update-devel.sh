d092f5c23ee742e7585c8fadd7b993c80000c1f7
Bitals <me@bitals.xyz>
2023-12-04 21:32:03 +0400

    All that was redundant, wiki literally says -o runs pkgver()

--- update-devel.sh
+++ update-devel.sh
@@ -5,9 +5,9 @@ sudo pacman -Syy
 echo Updating devel packages...
 
 repover=$( pacman -Si "$AURPACKAGE"|grep Version|sed -E 's/Version         : //' )
-cd /home/builder/.cache/aurutils/sync/"$AURPACKAGE"|| /opt/rebuilder.sh
+cd /home/builder/.cache/aurutils/sync/"$AURPACKAGE"|| echo "No local source found, rebuilding" && /opt/rebuilder.sh
 makepkg -o
-gitver=$( bash -c "source PKGBUILD && echo ${pkgver//%}" )
+gitver=$( grep pkgver= PKGBUILD|cut -d "=" -f 2 )
 echo Repo: $repover
 echo Source: $gitver
 if [[ "$repover" != "$gitver" ]]; then
