4fa1a60df63e5796bcd8a5822c871f9541af0bb8
Bitals <me@bitals.xyz>
2023-12-05 00:24:36 +0400

    Add pkgrel to testing. TODO: Epoch

--- update-devel.sh
+++ update-devel.sh
@@ -4,14 +4,14 @@ echo Updating pacman databases...
 sudo pacman -Syy
 echo Updating devel packages...
 
-repover=$( pacman -Si $AURPACKAGE|grep Version|sed -E 's/Version         : //'|cut -d "-" -f 1 )
+repover=$( pacman -Si $AURPACKAGE|grep Version|sed -E 's/Version         : //' )
 if [ ! -d /home/builder/.cache/aurutils/sync/"$AURPACKAGE" ]; then
     cd /home/builder/.cache/aurutils/sync/|| exit 1
     git clone https://aur.archlinux.org/"$AURPACKAGE".git || exit 1
 fi
 cd /home/builder/.cache/aurutils/sync/"$AURPACKAGE" || exit 1
 makepkg -do
-gitver=$( grep pkgver= PKGBUILD|cut -d "=" -f 2 )
+gitver=$( grep pkgver= PKGBUILD|cut -d "=" -f 2 )-$( grep pkgrel= PKGBUILD|cut -d "=" -f 2 )
 echo Repo: $repover
 echo Source: $gitver
 if [[ -n "$repover" && "$repover" != "$gitver" ]]; then
