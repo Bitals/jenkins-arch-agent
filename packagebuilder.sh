ee801bbe3bd1879bd6da9a95630f7e1f841492ec
Bitals <me@bitals.xyz>
2024-02-19 14:50:03 +0400

    Force rebuild git packages on PKGBUILD update because they just don't update pkgrel

--- packagebuilder.sh
+++ packagebuilder.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 echo Building "$AURPACKAGE"...
-aur sync -A --noconfirm --noview --sign --database Bitals --root /home/builder/bitalsrepo $AURPACKAGE || exit 1
-echo $(grep -m 1 epoch= /home/builder/.cache/aurutils/sync/$AURPACKAGE/PKGBUILD|cut -d = -f 2):$(grep -m 1 pkgver= /home/builder/.cache/aurutils/sync/$AURPACKAGE/PKGBUILD|cut -d = -f 2)-$(grep -m 1 pkgrel= /home/builder/.cache/aurutils/sync/$AURPACKAGE/PKGBUILD|cut -d = -f 2) > jenkins-ver-info
-#sudo kill $( cat /opt/piavpn-manual/pia_pid )
+if [ $( echo "$AURPACKAGE"|rev|cut -d "-" -f 1|rev ) == "git" ]; then
+    /opt/rebuilder.sh || exit 1
+else
+  aur sync -A --noconfirm --noview --sign --database Bitals --root /home/builder/bitalsrepo "$AURPACKAGE" || exit 1
+  echo $(grep -m 1 epoch= /home/builder/.cache/aurutils/sync/"$AURPACKAGE"/PKGBUILD|cut -d = -f 2):$(grep -m 1 pkgver= /home/builder/.cache/aurutils/sync/"$AURPACKAGE"/PKGBUILD|cut -d = -f 2)-$(grep -m 1 pkgrel= /home/builder/.cache/aurutils/sync/"$AURPACKAGE"/PKGBUILD|cut -d = -f 2) > jenkins-ver-info
+fi
+  #sudo kill $( cat /opt/piavpn-manual/pia_pid )
