1666ffa163af4a1e3a8992a6f15dd5ff678df76f
Bitals <me@bitals.xyz>
2024-03-05 12:46:30 +0400

    ignore optdeps

--- custombuilder.sh
+++ custombuilder.sh
@@ -11,7 +11,7 @@ cd /home/builder/workspace/AUR/"$OWNPACKAGE" || exit 1
 # rm -rf /home/builder/
 # rm -rf /home/builder/
 deps=""
-for i in $( grep "depends" .SRCINFO|cut -d "=" -f 2|cut -d ":" -f 1 ); do
+for i in $( grep "depends" .SRCINFO|grep -v optdepends|cut -d "=" -f 2|cut -d ":" -f 1 ); do
     deps+="$i "
 done
 sudo pacman -S --noconfirm $deps
